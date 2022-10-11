f1d9d2f71a5004e958d54ffd4b69ab5fd7d7d01a
Andy Humes <andrew.humes@du.edu>
2022-10-11 16:05:51 -0600

    stylingfooter

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,25 +1,45 @@
-import * as React from 'react';
-import Container from "@mui/material/Container";
-import Box from "@mui/material/Box";
 
 import "./footer.css";
 import resumeData from "../../resumeData.js";
 
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import { blueGrey } from "@mui/material/colors";
 
-
-
-function Footer() {
-  return(
- <>
-
- <Box className="footerBox">
-    
-    <h3>This is the Footer</h3>
+export default function Footer() {
+  return (
+    <Box className='footerBox' sx={{ flexGrow: 1 }}>
+      
+        
+        <Typography
+              variant="h5"
+              noWrap
+              sx={{
+                mr: 2,
+                display: { xs: "flex", md: "flex" },
+                flexGrow: 1,
+                fontFamily: "monospace",
+                fontWeight: 700,
+                letterSpacing: ".3rem",
+                color: "black",
+                textDecoration: "none",
+                justifyContent: "center",
+                alignItems: "center",
+                
+              }}
+            >
+              Portfolio developed by Andy Humes, 2022. 
+        </Typography>
+       
+      
     </Box>
-   
-    </>
-    
-  )
+  );
 }
 
-export default Footer;
+
