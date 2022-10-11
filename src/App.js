a2f287c3abc5f40305183402ee0566fc53772256
Andy Humes <andrew.humes@du.edu>
2022-10-11 17:07:01 -0600

    boilerbio

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 
 import TopApp from './components/topapp/TopApp';
+import Bio from './components/bio/Bio';
 import Footer from './components/footer/Footer';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
  <>
  
     <TopApp />
+    <Bio />
     <h1>This is the Body</h1>
     <h1>This is the Body</h1>
     <h1>This is the Body</h1>
---
+++ src/components/bio/Bio.js
@@ -0,0 +1,15 @@
+import * as React from "react";
+
+import "./bio.css";
+
+function Bio() {
+  return (
+    <>
+      <div className="bioContainer">
+        <h3>this is the bio</h3>
+      </div>
+    </>
+  );
+}
+
+export default Bio;
