c8bc70e7fcf871d499c103fa0fc1abed0dc8bb77
Andy Humes <andrew.humes@du.edu>
2022-10-11 12:08:04 -0600

    addedtopbarfrommui

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 
+import TopApp from './components/topapp/TopApp';
+
+
 function App() {
-  return <Button variant="contained">Hello World</Button>;
+  return(
+ <>
+ 
+    <TopApp />
+    <h1>This is the Body</h1>
+    </>
+  )
 }
 
 export default App;
