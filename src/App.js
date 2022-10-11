d773c6bad7a488e23e6eda78e61198b55d251ba0
Andy Humes <andrew.humes@du.edu>
2022-10-11 15:30:05 -0600

    boilerfooter

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import * as React from 'react';
 
 
 import TopApp from './components/topapp/TopApp';
-
+import Footer from './components/footer/Footer';
 
 function App() {
   return(
@@ -10,6 +10,8 @@ function App() {
  
     <TopApp />
     <h1>This is the Body</h1>
+   
+  <Footer />
     </>
   )
 }
